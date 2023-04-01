d3a70d0e91804ffb40b262d7ee7c94ef1f29e5cd
enkud <enkudinov222@gmail.com>
2023-04-01 13:21:06 +0300

    дз сделано

--- src/Book.java
+++ src/Book.java
@@ -6,7 +6,9 @@ public class Book {
     public Book(String nameBook, int age) {
         this.nameBook = nameBook;
         this.age = age;
+
     }
+
 
     public String getNameBook() {
         return this.nameBook;
@@ -15,6 +17,7 @@ public class Book {
     public int getAge() {
         return this.age;
     }
+
 
     public void setAge(int age) {
         this.age = age;
--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,17 @@ task1();
     }
     public static void task1 () {
         Book Teenager = new Book("Подросток", 1875);
-        Author Teenager1 = new Author("Федор", "Достоевский");
         System.out.println("Teenager.getNameBook() = " + Teenager.getNameBook());
         System.out.println("Teenager.getAge() = " + Teenager.getAge());
-        System.out.println("Teenager1.getAuthor() = " + Teenager1.getAuthor() + " " + Teenager1.getAuthorSurname());
+        Author Teenager1 = new Author("Федор", "Достоевский");
+        System.out.println("Teenager1.getAuthor() = " + Teenager1.getAuthor());
+        System.out.println("Teenager1.getAuthorSurname() = " + Teenager1.getAuthorSurname());
         Book Auditor = new Book("Ревизор", 1836);
-        Author Auditor1 = new Author("Николай", "Гоголь");
         System.out.println("Auditor.getNameBook() = " + Auditor.getNameBook());
         System.out.println("Auditor.getAge() = " + Auditor.getAge());
-        System.out.println("Auditor1.getAuthor() = " + Auditor1.getAuthor() + " " + Auditor1.getAuthorSurname());
+        Author Book1 = new Author("Николай", "Гоголь");
+        System.out.println("Book1.getAuthor() = " + Book1.getAuthor());
+        System.out.println("Book1.getAuthorSurname() = " + Book1.getAuthorSurname());
         Teenager.setAge(1900);
         Auditor.setAge(2000);
         System.out.println("Teenager.getAge() = " + Teenager.getAge());
